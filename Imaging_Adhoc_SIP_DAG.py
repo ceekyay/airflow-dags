2048e82b8c307258cc18d1b8c7d6d8013320a70c
ceekyay <ckovuri16@gmail.com>
2020-05-11 09:44:39 -0700

    test

---
+++ Imaging_Adhoc_SIP_DAG.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+from builtins import range
+from airflow.models import DAG
+import airflow
+from airflow.operators.bash_operator import BashOperator
+
+args = {
+    'owner': 'airflow',
+    'provide_context': True,
+    'start_date': datetime(2019, 11, 20),
+}
+
+dag = DAG(
+    dag_id='Imaging_Adhoc_SIP_Workflow',
+    default_args=args,
+    schedule_interval=None,
+    catchup=False,
+)
+
+Ingestion = BashOperator(
+  task_id='Ingestion',
+  bash_command="echo {{ dag_run.conf['ingestion_status'] }}",
+  dag=dag,
+)
+
+VirusCheck = BashOperator(
+  task_id='VirusCheck',
+  bash_command="echo {{ dag_run.conf['quarantine_bucket'] }}",
+  dag=dag,
+)
+
+MoveToPrecurated = BashOperator(
+  task_id='MoveToPrecurated',
+  bash_command="echo {{ dag_run.conf['precurated_bucket'] }}",
+  dag=dag,
+)
+
+FlywheelUpload = BashOperator(
+  task_id='FlywheelUpload',
+  bash_command="echo {{ dag_run.conf['fw_group'] }} && echo {{ dag_run.conf['fw_project'] }} && echo {{ dag_run.conf['fw_template'] }}",
+  dag=dag,
+)
+
+SuccessNotification = BashOperator(
+  task_id="SuccessNotification",
+  bash_command="echo {{ dag_run.conf['email_list'] }}",
+  dag=dag,
+)
+
+Ingestion >> VirusCheck >> MoveToPrecurated >> FlywheelUpload >> SuccessNotification
